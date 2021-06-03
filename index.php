dc21af690214181d376443f3fd0fc0a67869e012
ART_mj1 <mjmjpc@naver.com>
2021-06-03 23:14:10 +0900

    06/03 schedule project  데이터베이스,API 등 % 나타내기까지 완료 및 1차 업그레이드 완료

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Portfolio Process</title>
+
+ <!-- Favicon Link -->
+ <link rel="shortcut icon" href="/schedule/images/favicon.ico" type="image/x-icon">
+ <link rel="icon" href="/schedule/images/favicon.ico" type="image/x-icon">
+ <link rel="apple-touch-icon" href="/schedule/images/favicon.ico">
+
+ <!-- Font Awesome Link -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+ <!-- Google Font Link -->
+ <link rel="preconnect" href="https://fonts.gstatic.com">
+ <link
+  href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&family=Roboto:wght@100;300;400;500;700&display=swap"
+  rel="stylesheet">
+
+ <!-- Reset CSS Link -->
+ <link rel="stylesheet" href="/schedule/css/reset.css">
+
+ <!-- Plugin CSS Link -->
+ <link rel="stylesheet" href="/schedule/lib/css/lightslider.css">
+ <link rel="stylesheet" href="/schedule/lib/css/piechart.css">
+
+ <!-- Main CSS Link -->
+ <link rel="stylesheet" href="/schedule/css/style.css">
+
+ <!-- Animation CSS Link -->
+ <link rel="stylesheet" href="/schedule/css/animation.css">
+
+ <!-- Media Query CSS Link -->
+ <link rel="stylesheet" href="/schedule/css/Media.css">
+</head>
+
+<body>
+
+ <div class="wrapper">
+  <!-- Main Dashboard Frame -->
+  <div class="dashboard">
+
+   <header>
+    <h2><a href="#"><i class="custom-font"></i></a></h2>
+    <ul class="gnb">
+     <li><a href="#"><i class="fa fa-trello"></i></a></li>
+     <li><a href="#"><i class="fa fa-pencil"></i></a></li>
+     <li><a href="#"><i class="fa fa-search"></i></a></li>
+    </ul>
+    <a href="#" class="sign-out"><i class="fa fa-sign-out"></i></a>
+
+    <div class="mobile-menu">
+     <span></span>
+     <span></span>
+    </div>
+   </header>
+
+   <section class="graph-ui">
+    <div class="intro">
+     <div class="slide-box">
+      <h2>Database Project Process</h2>
+      <p>데이터베이스 테이블 설계 완료.<br>테이블 UI 디자인 완료</p>
+      <a href="#">More Details</a>
+      <i class="fa fa-database"></i>
+     </div>
+     <div class="slide-box">
+      <h2>Database Project Process</h2>
+      <p>API 테이블 설계 완료.<br>테이블 UI 디자인 완료</p>
+      <a href="#">More Details</a>
+      <i class="fa fa-database"></i>
+     </div>
+     <div class="slide-box">
+      <h2>Database Project Process</h2>
+      <p>리뉴얼 테이블 설계 완료.<br>테이블 UI 디자인 완료</p>
+      <a href="#">More Details</a>
+      <i class="fa fa-database"></i>
+     </div>
+     <div class="slide-box">
+      <h2>Database Project Process</h2>
+      <p>기획 테이블 설계 완료.<br>테이블 UI 디자인 완료</p>
+      <a href="#">More Details</a>
+      <i class="fa fa-database"></i>
+     </div>
+    </div>
+    <div class="each-pofol">
+     <div>
+      <div class="each-title">
+       <h3>Each Portfolio Process Rate</h3>
+      </div>
+      <div class="each-graph">
+       <div class="db-pofol">
+        <span class="chart" data-percent="86">
+         <span class="percent"></span>
+        </span>
+        <b>DB Project</b>
+        <i class="fa fa-database"></i>
+       </div>
+       <div class="api-pofol">
+        <span class="chart" data-percent="60">
+         <span class="percent"></span>
+        </span>
+        <b>API Project</b>
+        <i class="fa fa-thermometer-half"></i>
+       </div>
+       <div class="renewal-pofol">
+        <span class="chart" data-percent="50">
+         <span class="percent"></span>
+        </span>
+        <b>Renewal Project</b>
+        <i class="fa fa-clone"></i>
+       </div>
+       <div class="planning-pofol">
+        <span class="chart" data-percent="35">
+         <span class="percent"></span>
+        </span>
+        <b>Planning Project</b>
+        <i class="fa fa-bar-chart-o"></i>
+       </div>
+      </div>
+     </div>
+    </div>
+    <div class="total-pofol">
+     <div class="total-chart">
+      <span class="chart" data-percent="35">
+       <span class="percent"></span>
+       <h3>Total Process Rate</h3>
+      </span>
+
+     </div>
+     <div class="total-txt">
+
+     </div>
+    </div>
+   </section>
+   <section class="table-ui">
+    <div class="new-update">
+     <div class="tit-box">
+      <p>Recent Update</p>
+      <a href="#">More</a>
+     </div>
+     <ul class="con-details">
+      <li>
+       <i class="fa fa-database"></i>
+       <div class="con-txt">
+        <p><a href="#">데이터베이스 테이블 설계 완료</a></p>
+        <em>2021-05-31</em>
+       </div>
+      </li>
+      <li>
+       <i class="fa fa-database"></i>
+       <div class="con-txt">
+        <p><a href="#">데이터베이스 테이블 설계 완료</a></p>
+        <em>2021-05-31</em>
+       </div>
+      </li>
+      <li>
+       <i class="fa fa-database"></i>
+       <div class="con-txt">
+        <p><a href="#">데이터베이스 테이블 설계 완료</a></p>
+        <em>2021-05-31</em>
+       </div>
+      </li>
+     </ul>
+    </div>
+    <div class="each-contents">
+     <div class="each-btns">
+      <button class="active">Database</button>
+      <button>API</button>
+      <button>Renewal</button>
+      <button>Planning</button>
+     </div>
+     <ul class="con-details">
+      <li>
+       <i class="fa fa-database"></i>
+       <div class="con-txt">
+        <p><a href="#">데이터베이스 테이블 설계 완</a></p>
+        <em>2021-05-31</em>
+       </div>
+      </li>
+      <li>
+       <i class="fa fa-database"></i>
+       <div class="con-txt">
+        <p><a href="#">데이터베이스 테이블 설계 완료</a></p>
+        <em>2021-05-31</em>
+       </div>
+      </li>
+      <li>
+       <i class="fa fa-database"></i>
+       <div class="con-txt">
+        <p><a href="#">데이터베이스 테이블 설계 완료</a></p>
+        <em>2021-05-31</em>
+       </div>
+      </li>
+     </ul>
+    </div>
+   </section>
+
+  </div>
+  <!-- End of Main Dashboard Frame -->
+ </div>
+
+ <!-- Jquery Framework Load -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <!-- Plugins Load -->
+ <script src="/schedule/lib/js/lightslider.js"></script>
+ <script src="/schedule/lib/js/easypiechart.js"></script>
+ <!-- Vanilla JS Code Load -->
+ <script src="/schedule/js/index.js"></script>
+ <!-- jQuery Code Load -->
+ <script src="/schedule/js/jquery.index.js"></script>
+
+</body>
+
+</html>
